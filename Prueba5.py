077c9f7f84cd500a3191a5cfc6f2b37c8d2b96b9
Nicolas Tacas <ngeort@gmail.com>
2019-09-03 10:26:06 -0500

    lista de objetos en descripcion

--- Prueba5.py
+++ Prueba5.py
@@ -1,6 +1,6 @@
 """App como herramienta de revisión de scripts consolidados para QAI"""
 
-#Importaciones
+# Importaciones
 ##from tkinter import *
 from tkinter import Frame
 from tkinter import Menu
@@ -19,12 +19,12 @@ import getpass
 import os
 import errno
 
-#Usuario por default
+# Usuario por default
 user = getpass.getuser()
 
 select = 'select'
 workflow = 'n usp_'
-        
+
 function = 'ufn_'
 equal = '='
 objectid_ = 'object_id'
@@ -38,7 +38,7 @@ lineComment = '--'
 blockCommentOpen = '/*'
 blockCommentClose = '*/'
 
-##constantes
+# constantes
 
 space = ' '
 
@@ -59,153 +59,184 @@ exec_ = 'exec'
 grant = 'grant'
 execute = 'execute'
 ##on = 'on '
-##to = 'to '}
+# to = 'to '}
 beg = 0
-        
-class Window(Frame):
-  
-    #Inicialización
-    def __init__(self, master):
-        #Parametros para mi clase Frame
-        Frame.__init__(self, master)   
-        #Referenciamos             
-        self.master = master
-        #corremos
-        self.init_window()
-        
-    def init_window(self):
-        #Titulo de la ventana
-        self.master.title("QA Tools")
-        #Instanciamos un menu
-        menu = Menu(self.master)
-        #Referenciamos al widget
-        self.master.config(menu=menu)
-        #Agreamos submenus
-        file = Menu(menu, tearoff = 0)
-        #Agregamos submenu Open
-        file.add_command(label="Open", command=self.openFile)
-        #Agregamos submenu exit
-        file.add_command(label="Exit", command=self.client_exit)
-        #menu File
-        menu.add_cascade(label="File", menu=file)
-        ##Scrollbar
-        self.scrollbar = Scrollbar(self.master, orient=VERTICAL)  
-        self.scrollbar.pack( side = RIGHT, fill = Y )
-        ##Lista                
-        self.lista = Listbox(self.master, font=("Helvetica", 8), borderwidth=0, activestyle=NONE, yscrollcommand = self.scrollbar.set)     
-        self.lista.pack(fill=BOTH, expand=1)      
-        self.scrollbar.config( command = self.lista.yview )
 
-    #Funcion para cerrar la app
+datos = 'datos'
+generales = 'generales'
+parametros= 'parametros'
+control = 'control'
+version = 'version'
+historial = 'historial'
+autor = 'autor'
+fecha = 'fecha'
+
+class Window(Frame):
+
+    # Inicialización
+    def __init__(self, master):
+        # Parametros para mi clase Frame
+        Frame.__init__(self, master)
+        # Referenciamos
+        self.master = master
+        # corremos
+        self.init_window()
+
+    def init_window(self):
+        # Titulo de la ventana
+        self.master.title("QA Tools")
+        # Instanciamos un menu
+        menu = Menu(self.master)
+        # Referenciamos al widget
+        self.master.config(menu=menu)
+        # Agreamos submenus
+        file = Menu(menu, tearoff=0)
+        # Agregamos submenu Open
+        file.add_command(label="Open", command=self.openFile)
+        # Agregamos submenu exit
+        file.add_command(label="Exit", command=self.client_exit)
+        # menu File
+        menu.add_cascade(label="File", menu=file)
+        # Scrollbar
+        self.scrollbar = Scrollbar(self.master, orient=VERTICAL)
+        self.scrollbar.pack(side=RIGHT, fill=Y)
+        # Lista
+        self.lista = Listbox(self.master, font=(
+            "Helvetica", 8), borderwidth=0, activestyle=NONE, yscrollcommand=self.scrollbar.set)
+        self.lista.pack(fill=BOTH, expand=1)
+        self.scrollbar.config(command=self.lista.yview)
+
+    # Funcion para cerrar la app
     def client_exit(self):
-        
+
         self.master.destroy()
 
-    #Funcion para abrir un doc   
+    # Funcion para abrir un doc
     def openFile(self):
 
-        self.lista.delete(0,END)
-        
-        ##obtenemos el archivo
+        self.lista.delete(0, END)
+
+        # obtenemos el archivo
         filepath = fdialog.askopenfilename(initialdir="C:/Users/%s" % user,
-                               filetypes =(("SQL File", "*.sql"),("All Files","*.*")),
-                               title = "Selección de Archivo")
+                                           filetypes=(
+                                               ("SQL File", "*.sql"), ("All Files", "*.*")),
+                                           title="Selección de Archivo")
 
         switchCommentOn = 'N'
         listBlockBD = [use]
-        listBlockHeader = [if_,exists,from_,sysobjects,objectid_,and_,type_,drop,procedure]
-        listBlockBody = [create,procedure]
-        listBlockFoot = [grant,execute]
+        listBlockHeader = [if_, exists, from_, sysobjects,
+                           objectid_, and_, type_, drop, procedure]
+        listBlockBody = [create, procedure]
+        listBlockFoot = [grant, execute]
         dic = {}
         dicBlock = {}
         dicParameters = {}
+        dicBlockDescriptions = {}
 
         listObjects = []
-        listConstants = [use,if_,exists,from_,sysobjects,objectid_,and_,type_,drop,procedure,create,grant,execute]
-        listVariables = [usp]
+        listConstants = [use, if_, exists, from_, sysobjects, objectid_,
+                         and_, type_, drop, procedure, create, grant, execute]
+        listStatements = [usp]
         listParameters = ['@']
+        listDescriptions = [datos,generales,parametros,control,version,historial,autor,fecha]
         listBlock = []
-        conjBlockBD=set(listBlockBD)
-        conjBlockHeader=set(listBlockHeader)
-        conjBlockBody=set(listBlockBody)
-        conjBlockFoot=set(listBlockFoot)
+        conjBlockBD = set(listBlockBD)
+        conjBlockHeader = set(listBlockHeader)
+        conjBlockBody = set(listBlockBody)
+        conjBlockFoot = set(listBlockFoot)
 
         listBlockParameters = []
+        listBlockDescriptions = []
 
         try:
-            ##con el nombre del archivo
+            # con el nombre del archivo
             with open(filepath) as fp:
-                ##obtenemos la primera linea
+                # obtenemos la primera linea
                 line = fp.readline()
                 cnt = 1
-                
-                ##mientras exista una linea
-                while line:
-                    ##damos formato estandar a la linea reemplazando )([].' por un espacio
-                    line = line.lower().replace(')',' ').replace('(',' ').replace('[',' ').replace(']',' ').replace('.',' ').replace("'",' ')
 
-                    switchBlockOn = 'S'
-                    
+                # mientras exista una linea
+                while line:
+                    # damos formato estandar a la linea reemplazando )([].' por un espacio
+                    line = line.lower().replace(')', ' ').replace('(', ' ').replace('[', ' ').replace(
+                        ']', ' ').replace('.', ' ').replace("'", ' ').replace("\n", ' ').replace("\t", ' ')
+
                     strippedLine = line.strip()
 
                     if strippedLine == "":
                         line = fp.readline()
                         cnt += 1
                         continue
-                    
+
                     if strippedLine == go:
-                        
+
                         conjBlock = set(listBlock)
-                            
+
                         if conjBlock == conjBlockBD:
                             print(conjBlock)
-                                
                         elif conjBlockHeader.issubset(conjBlock):
                             print(conjBlock)
-                            print(conjBlock - conjBlockHeader)
-                            print(dic.keys())
                         elif conjBlockBody.issubset(conjBlock):
                             print(conjBlock)
-                            print(dicParameters)
                         elif conjBlockFoot.issubset(conjBlock):
                             print(conjBlock)
 
-                        switchBlockOn = 'N'
-                        listBlock.clear()
+
+                        print (listBlockDescriptions)
+                        
+                        listBlock.clear()  # aca se llenan constantes y variables
                         listBlockParameters.clear()
+                        listBlockDescriptions.clear()
 
                     else:
+
+                        indexBlockComentOpen = line.find(blockCommentOpen, beg)
+
+                        if indexBlockComentOpen >= 0 or switchCommentOn == 'S':
+                            indexBlockComentClose = line.find(
+                                blockCommentClose, beg)
+                            if indexBlockComentClose >= 0:
+                                switchCommentOn = 'N'
+                            else:
+                                for const in listDescriptions:
+                                    startPosition = line.find(const, beg)
+                                    while startPosition >= 0:
+                                        endPosition = line.find(space, startPosition)
+                                        object_ = line[startPosition: endPosition]
+                                        if object_ in listDescriptions:
+                                            # listObjects.append(object_)
+                                            if object_ in listBlockDescriptions:
+                                                dicBlockDescriptions[object_] += 1
+                                            else:
+                                                dicBlockDescriptions[object_] = 1
+                                                listBlockDescriptions.append(object_)
+
+                                        startPosition = line.find(const, endPosition + 1)
+
+                                switchCommentOn = 'S'
+                        
+
                         for const in listConstants:
                             startPosition = line.find(const, beg)
                             while startPosition >= 0:
-                                endPosition = line.find(space,startPosition)
-                                object_     = line[startPosition: endPosition]
+                                endPosition = line.find(space, startPosition)
+                                object_ = line[startPosition: endPosition]
                                 if object_ in listConstants:
-                                    #listObjects.append(object_)
-                                    
+                                    # listObjects.append(object_)
                                     if object_ in listBlock:
                                         dicBlock[object_] += 1
                                     else:
                                         dicBlock[object_] = 1
                                         listBlock.append(object_)
-                                
-                                startPosition = line.find(const, endPosition + 1)
-                            
-                            """ startPosition = line.find(const)
-                            
-                            if startPosition >= 0:
-                                endPosition = line.find(space,startPosition)
-                                object_     = line[startPosition: endPosition]
-                                if object_ in listConstants:
-                                    #listObjects.append(object_)
-                                    listBlock.append(object_) """
 
-                        for var in listVariables:
+                                startPosition = line.find(const, endPosition + 1)
+
+                        for var in listStatements:
                             startPosition = line.find(var, beg)
                             while startPosition >= 0:
-                                endPosition = line.find(space,startPosition)
-                                object_     = line[startPosition: endPosition]
-                                #listObjects.append(object_)
+                                endPosition = line.find(space, startPosition)
+                                object_ = line[startPosition: endPosition]
+                                # listObjects.append(object_)
                                 if object_ in listBlock:
                                     dic[object_] += 1
                                 else:
@@ -217,9 +248,9 @@ class Window(Frame):
                         for par in listParameters:
                             startPosition = line.find(par)
                             while startPosition >= 0:
-                                endPosition = line.find(space,startPosition)
-                                object_     = line[startPosition: endPosition]
-                                #listObjects.append(object_)
+                                endPosition = line.find(space, startPosition)
+                                object_ = line[startPosition: endPosition]
+                                # listObjects.append(object_)
                                 if object_ in listBlockParameters:
                                     dicParameters[object_] += 1
                                 else:
@@ -227,19 +258,8 @@ class Window(Frame):
                                     listBlockParameters.append(object_)
 
                                 startPosition = line.find(par, endPosition + 1)
-                            
-                    """ print (listObjects) """
 
-                    """ if switchBlockOn == 'S':
-                        for targetObj in listObjects:
-
-                            if targetObj in listBlock:
-                                dic[targetObj] += 1
-                            else:
-                                dic[targetObj] = 1
-                                listBlock.append(targetObj) """
-
-                    ##siguiente linea        
+                    # siguiente linea
                     line = fp.readline()
                     cnt += 1
 
@@ -247,8 +267,9 @@ class Window(Frame):
             if e.errno == errno.ENOENT:
                 messagebox.showwarning("Warning", "File not found / File not selected")
 
+
 def main():
-  
+
     root = tk.Tk()
     root.iconbitmap('kms.ico')
     root.geometry("400x300+300+300")
@@ -259,11 +280,11 @@ def main():
 if __name__ == '__main__':
     main()
 
-## Developer mode
-##        if not os.path.isfile(filepath):
+# Developer mode
+# if not os.path.isfile(filepath):
 ##            messagebox.showinfo("Info", "You must to choose a file")
-##                            if storeObject == 'usp_plantillametodoensayemuestraqc_select':
+# if storeObject == 'usp_plantillametodoensayemuestraqc_select':
 ##                                print (line)
-##            items = self.listbox.get(0, tk.END)      
-##        except Exception as inst:
+##            items = self.listbox.get(0, tk.END)
+# except Exception as inst:
 ##            messagebox.showinfo("Info", inst)
